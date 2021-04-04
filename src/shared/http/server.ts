47337cdb486262b2e54701d3047a59c2d1133372
Abel Souza <abelsouzacosta@gmail.com>
2021-04-04 11:41:15 -0300

    feat(server): adicionando configurações
    Foram adicionadas as configurações do typeorm bem como o router principal da
    aplicação

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,31 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import AppError from '../errors/AppError';
+import router from './routes/';
+import '@shared/typeorm';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(router);
+
+// middleware de erros da aplicação
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(400).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
+);
 
 app.listen(3030);
