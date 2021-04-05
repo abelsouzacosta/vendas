ed34c74386d05f55fd227c7b020d90e796410ddc
Abel Souza <abelsouzacosta@gmail.com>
2021-04-05 18:59:34 -0300

    fix(users): migration com campo incorreto
    mudando o nome do campo udpated_at -> udpated_at
    impedia de fazer inserções de usuários

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import productRouter from '@modules/products/routes/products.routes';
+import userRouter from '@modules/users/routes/users.routes';
 
 const router = Router();
 
 router.use('/products', productRouter);
+router.use('/users', userRouter);
 
 router.get('/', (request, response) => {
   return response.status(200).send({
