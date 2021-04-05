9b1feeddcab3318fcc491879cf31b5d6f3c47ce8
Abel Souza <abelsouzacosta@gmail.com>
2021-04-05 17:46:49 -0300

    feat(user): permite realizar operações com a entidade de usuário
    Adicionando migration, entidade e repositório do domínio user

---
+++ src/modules/users/typeorm/entities/User.ts
@@ -0,0 +1,28 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  udpated_at: Date;
+}
