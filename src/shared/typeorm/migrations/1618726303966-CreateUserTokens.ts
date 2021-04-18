cf35211fc32edd6ff3226bdd01c990c3ccbe5ee2
Abel Souza <abelsouzacosta@gmail.com>
2021-04-18 03:27:52 -0300

    chore(user/password-reset): Adicionando migration
    Migration da tabela user_tokens responsável por devolver ao usuário
    um token gerado para que seja possível recuperar a senha
    O token gerado será enviado por email e com a validação desse token será
    possível então recuperar a senha do usuário

---
+++ src/shared/typeorm/migrations/1618726303966-CreateUserTokens.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUserTokens1618726303966 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_tokens',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'token',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'is_used',
+            type: 'boolean',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user_tokens');
+  }
+}
