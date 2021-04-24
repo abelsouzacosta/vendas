9e14404974d0c7c564bb15565e2b19b7bbfc5fc5
Abel Souza <abelsouzacosta@gmail.com>
2021-04-24 19:52:41 -0300

    feat(rate limiter): adicionando rate limiter
    O rate limiter é responsável por limitar o n[umero de requisições feitas
    a api, no caso dessa aplicação são feitas no máximo
    5 requisições por segundo

---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from 'express';
+import redis from 'redis';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import AppError from '@shared/errors/AppError';
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASS,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'ratelimit',
+  points: 5,
+  duration: 1,
+  // número de requisições por segundo por ip
+  // cada endereço de ip pode fazer no máximo 5 requisições por segundo
+  // por ip
+});
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(request.ip);
+
+    next();
+  } catch (error) {
+    throw new AppError('Too many request', 429);
+  }
+}
