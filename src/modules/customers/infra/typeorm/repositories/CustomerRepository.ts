cc4ada5236d4d0d8b589b0b1b48e0308a90cdb74
Abel Souza <abelsouzacosta@gmail.com>
2021-04-24 15:14:34 -0300

    chore: implementando métodos
    Implementando os métodos save, find e create no CustomerRepository
    modified: src/modules/customers/infra/typeorm/repositories/CustomerRepository.ts

--- src/modules/customers/infra/typeorm/repositories/CustomerRepository.ts
+++ src/modules/customers/infra/typeorm/repositories/CustomerRepository.ts
@@ -1,10 +1,17 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { ICustomerRepository } from '@modules/customers/domain/repositories/ICustomersRepository';
 import { Customer } from '../entities/Customer';
+import { getRepository, Repository } from 'typeorm';
+import { ICustomerCreate } from '@modules/customers/domain/models/ICustomerCreate';
 
-@EntityRepository(Customer)
-export class CustomerRepository extends Repository<Customer> {
+export class CustomerRepository implements ICustomerRepository {
+  private ormRepository: Repository<Customer>;
+
+  constructor() {
+    this.ormRepository = getRepository(Customer);
+  }
+
   public async findById(id: string): Promise<Customer | undefined> {
-    const customer = this.findOne({
+    const customer = this.ormRepository.findOne({
       where: {
         id,
       },
@@ -14,12 +21,44 @@ export class CustomerRepository extends Repository<Customer> {
   }
 
   public async findByEmail(email: string): Promise<Customer | undefined> {
-    const customer = this.findOne({
+    const customer = this.ormRepository.findOne({
       where: {
         email,
       },
     });
 
     return customer;
+  }
+
+  public async findByName(name: string): Promise<Customer | undefined> {
+    const customer = this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return customer;
+  }
+
+  public async create({ name, email }: ICustomerCreate): Promise<Customer> {
+    const customer = this.ormRepository.create({ name, email });
+
+    return customer;
+  }
+
+  public async save(customer: Customer): Promise<Customer> {
+    await this.ormRepository.save(customer);
+
+    return customer;
+  }
+
+  public async remove(customer: Customer): Promise<void> {
+    await this.ormRepository.remove(customer);
+  }
+
+  public async find(): Promise<Customer[] | undefined> {
+    const customers = this.ormRepository.find();
+
+    return customers;
   }
 }
